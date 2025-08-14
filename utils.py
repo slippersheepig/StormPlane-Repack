b12f0339154241ac7eefde10bf7a22545007d729
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-08-14 14:39:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,5 @@
-import random
-
 def rects_collide(a, b):
-    """
-    矩形碰撞检测
-    a, b: 对象需有 x, y, width, height 属性
-    """
+    """检测两个矩形是否碰撞"""
     return not (
         a.x + a.width < b.x or
         a.x > b.x + b.width or
@@ -12,14 +7,6 @@ def rects_collide(a, b):
         a.y > b.y + b.height
     )
 
-def random_position(canvas_width, sprite_width):
-    """
-    在画布宽度范围内随机生成 X 坐标
-    """
-    return random.randint(0, canvas_width - sprite_width)
-
 def clamp(value, min_value, max_value):
-    """
-    限制数值范围
-    """
+    """限制 value 在 [min_value, max_value] 范围内"""
     return max(min_value, min(value, max_value))
